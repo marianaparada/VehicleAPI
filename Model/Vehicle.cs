76eef111ccad033966356bc1d80e7ec9fe97db67
Mariana Parada <a2024111530@estudiantes.upsa.edu.bo>
2025-09-01 20:03:08 -0400

    crear models y vehicles

--- Model/Vehicle.cs
+++ Model/Vehicle.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
 namespace VehicleAPI.Model;
 
 public class Vehicle
 {
-    
+    public Guid Id { get; set; } = Guid.NewGuid();
+    [Required, StringLength(50)] 
+    public string Make { get; init; } = null!;
+    [Required, StringLength(50)]
+    public string Model { get; init; } = null!;
+    [Range(1900,2100)]
+    public int Year { get; init; }
 }
